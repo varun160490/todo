95eccfd6941c6462e9f1598be2baa6bb1c4166e5
Varun <8827524+varun160490@users.noreply.github.com>
2021-07-17 01:10:24 +0530

    view files added

---
+++ view/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login';
+import signup from './pages/signup';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+            <Route exact path="/login" component={login}/>
+            <Route exact path="/signup" component={signup}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+export default App;
